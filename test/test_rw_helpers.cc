d1db04a2c11061ac531142aed405d7d40fc6e69a
Sam Yates <halfflat@gmail.com>
2025-09-17 21:27:21 +0300

    Fix rw numeric tests.

--- test/test_rw_helpers.cc
+++ test/test_rw_helpers.cc
@@ -8,39 +8,36 @@ using namespace parapara;
 template <typename V>
 using repn_tbl = std::pair<V, std::string>[];
 
+template <typename V>
+using as_array = V[];
 
 TEST(parapara, rw_numeric) {
-    // integral types
-
-    auto expect_repn = [](const auto& tbl) {
+    auto expect_repn = [](const auto& tbl, bool skip_write_fallback = false) {
         for (auto [n, repn]: tbl) {
             using T = decltype(n);
-            hopefully<T> hn = read_numeric_fallback<T>(repn);
-            EXPECT_TRUE(hn) << "read_numeric_fallback failed on '" << repn << '"';
-            if (hn) {
-                EXPECT_EQ(n, hn.value());
+
+            {
+                hopefully<T> hn = read_numeric_fallback<T>(repn);
+                EXPECT_TRUE(hn) << "read_numeric_fallback failed on '" << repn << '"';
+                if (hn) EXPECT_EQ(n, hn.value());
             }
 
             if constexpr (can_from_chars_v<T>) {
-                hn = read_cc<T>(repn);
+                hopefully<T> hn = read_cc<T>(repn);
                 EXPECT_TRUE(hn);
-                if (hn) {
-                    EXPECT_EQ(n, hn.value());
-                }
+                if (hn) EXPECT_EQ(n, hn.value());
             }
 
-            hopefully<std::string> hs = write_numeric_fallback(n);
-            EXPECT_TRUE(hs) << "write_numeric_fallback failed on '" << repn << '"';
-            if (hs) {
-                EXPECT_EQ(repn, hs.value());
+            if (!skip_write_fallback) {
+                hopefully<std::string> hs = write_numeric_fallback(n);
+                EXPECT_TRUE(hs) << "write_numeric_fallback failed on '" << repn << '"';
+                if (hs) EXPECT_EQ(repn, hs.value());
             }
 
             if constexpr (can_to_chars_v<T>) {
-                hs = write_cc(n);
+                hopefully<std::string> hs = write_cc(n);
                 EXPECT_TRUE(hs);
-                if (hs) {
-                    EXPECT_EQ(repn, hs.value());
-                }
+                if (hs) EXPECT_EQ(repn, hs.value());
             }
         }
     };
@@ -117,36 +114,65 @@ TEST(parapara, rw_numeric) {
         });
     }
 
+    // Skip write fallback tests for floating point types as
+    // the fallback routine doesn't guarantee shortest representation.
     {
         SCOPED_TRACE("numeric type: float");
         expect_repn(repn_tbl<float>{
-            {0., "0"},
-            {-0.25f, "-0.25"},
-            {83000000.f, "8.3e+07"}
-        });
+                {0., "0"},
+                {-0.25f, "-0.25"},
+                {83000000.f, "8.3e+07"}
+                }, true);
     }
 
     {
         SCOPED_TRACE("numeric type: double");
         expect_repn(repn_tbl<double>{
-            {0., "0"},
-            {-1./16384., "-6.103515625e-05"},
-            {21474000000., "2.1474e+10"}
-        });
+                {0., "0"},
+                {-1./16384., "-6.103515625e-05"},
+                {21474000000., "2.1474e+10"}
+                }, true);
     }
 }
 
-#if 0
-TEST(parapara, rw_numeric_round_trip) {
-    // TODO: expand on examples, test corner cases.
+TEST(parapara, rw_numeric_roundtrip) {
+    auto verify_roundtrip = [](const auto& ns) {
+        for (auto n: ns) {
+            using T = decltype(n);
+            {
+                hopefully<std::string> hs = write_numeric_fallback(n);
+                EXPECT_TRUE(hs) << "write_numeric_fallback failed on " << n;
+
+                if (hs) {
+                    hopefully<T> hn = read_numeric_fallback<T>(hs.value());
+                    EXPECT_TRUE(hn) << "read_numeric_fallback failed on '" << hs.value() << "'";
+                    EXPECT_EQ(n, hn.value());
+                }
+            }
+
+            if constexpr (can_to_chars_v<T> && can_from_chars_v<T>) {
+                hopefully<std::string> hs = write_cc(n);
+                EXPECT_TRUE(hs) << "write_cc failed on " << n;
+
+                if (hs) {
+                    hopefully<T> hn = read_cc<T>(hs.value());
+                    EXPECT_TRUE(hn) << "read_cc failed on '" << hs.value() << "'";
+                    EXPECT_EQ(n, hn.value());
+                }
+            }
+        }
+    };
+
     {
-        float vs[] = {-3.1f, -1.2345e30f, std::numeric_limits<float>::infinity() };
+        SCOPED_TRACE("numeric type: float");
+        verify_roundtrip(as_array<float>{-3.1f, -1.2345e30f, std::numeric_limits<float>::infinity()});
     }
     {
-        double vs[] = {-3.1, -1.2345e30, std::numeric_limits<double>::infinity() };
+        SCOPED_TRACE("numeric type: double");
+        verify_roundtrip(as_array<double>{-3.1f, -1.2345e-240, std::numeric_limits<double>::infinity()});
     }
     {
-        float vs[] = {-3.1f, -1.2345e30f, std::numeric_limits<long double>::infinity() };
+        SCOPED_TRACE("numeric type: long double");
+        verify_roundtrip(as_array<long double>{-3.1l, -1.2345e-240l, std::numeric_limits<long double>::infinity()});
     }
 }
-#endif
