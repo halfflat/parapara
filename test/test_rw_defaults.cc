51ec0ca9549ac5a0f6013fee58c604dc6a5b49bd
Sam Yates <halfflat@gmail.com>
2026-02-06 16:42:39 +1030

    Finish unit tests for write_qstring_conditional

--- test/test_rw_defaults.cc
+++ test/test_rw_defaults.cc
@@ -109,3 +109,69 @@ TEST(parapara, rw_qstring) {
 
     EXPECT_EQ("unquoted\\a"s, read_qstring(R"(unquoted\a)").value_or(""s));
 }
+
+TEST(parapara, write_qstring_conditional) {
+    using namespace std::literals;
+
+    // Without a delimiter test, expect quoting to occur only if
+    // 1. There is leading space.
+    // 2. There is a character that needs to be escaped.
+    // Escaped characters are those with code less than 32, the
+    // double quote character '"', and the backslash '\'.
+
+    auto write_qstring_csv = write_qstring_conditional{","};
+
+    std::string expect_unquoted[] = {
+        ""s,
+        "\177"s,    // [Del] isn't escaped.
+        "a  "s,     // Trailing space okay too.
+        "a b  d'"s  // Single quote fine.
+    };
+
+    for (auto& s: expect_unquoted) {
+        auto mq = write_qstring(s);
+        ASSERT_TRUE(mq);
+        EXPECT_EQ(s, mq.value());
+        mq = write_qstring_csv(s);
+        ASSERT_TRUE(mq);
+        EXPECT_EQ(s, mq.value());
+    }
+
+    std::string expect_quoted[] = {
+        " fop"s,    // Leading space.
+        "a\"b"s,    // Contains quote.
+        "\\b"s,     // Contains backslash.
+        "a\0b"s,    // Contains other escapable characters ....
+        "\001wat"s
+    };
+
+    for (auto& s: expect_quoted) {
+        auto q = write_qstring_always(s);
+        ASSERT_TRUE(q);
+        auto mq = write_qstring(s);
+        ASSERT_TRUE(mq);
+        EXPECT_EQ(q.value(), mq.value());
+        mq = write_qstring_csv(s);
+        ASSERT_TRUE(mq);
+        EXPECT_EQ(q.value(), mq.value());
+    }
+
+    // write_qstring_csv should quote strings with commas though.
+    EXPECT_EQ("\"a,b,c\""s, write_qstring_csv("a,b,c"s).value());
+    EXPECT_EQ("a,b,c"s, write_qstring("a,b,c"s).value());
+
+    // Confirm quoting of strings with ambiguous verminal fragments of
+    // multi-character delimiters.
+
+    auto wqsc__ = write_qstring_conditional{"__"};
+    EXPECT_EQ("\"a__b\""s, wqsc__("a__b"s).value());
+    EXPECT_EQ("\"a_b_\""s, wqsc__("a_b_"s).value());
+    EXPECT_EQ("\"_a_b\""s, wqsc__("_a_b"s).value());
+    EXPECT_EQ("a_b"s,      wqsc__("a_b"s).value());
+
+    auto wqsc1231 = write_qstring_conditional{"1231"};
+    EXPECT_EQ("\"_123\""s, wqsc1231("_123"s).value());
+    EXPECT_EQ("_12"s,      wqsc1231("_12"s).value());
+    EXPECT_EQ("\"231_\""s, wqsc1231("231_"s).value());
+    EXPECT_EQ("31_"s,      wqsc1231("31_"s).value());
+}
